052b4ec6e0b0d49c341d2771afe219d54b70d921
Fercho0506 <157646134+Fercho0506@users.noreply.github.com>
2025-04-06 18:26:23 -0500

    Delete Proyecto directory

--- Proyecto/src/Usuarios/Cliente.java
+++
@@ -1,8 +0,0 @@
-package Usuarios;
-
-public class Cliente extends Usuario{
-	
-	public Cliente(String login, String password, int edad, float altura) {
-		super(login, password, "cliente", edad, altura);
-	}
-}
